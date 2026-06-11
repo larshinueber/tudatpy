fa21c8ac97ee3d2db07e3a070d7136539758a8ac
Lars Hinüber <105521415+larshinueber@users.noreply.github.com>
2026-06-11 23:11:43 +0200

    allow for multiple tabulated atmospheric corrections simultaneously

--- src/tudat/simulation/estimation_setup/createAtmosphericLightTimeCorrection.cpp
+++ src/tudat/simulation/estimation_setup/createAtmosphericLightTimeCorrection.cpp
@@ -127,8 +127,21 @@ AtmosphericCorrectionPerStationAndSpacecraftType extractAtmosphericCorrection(
             {
                 for( const observation_models::ObservableType& observableType : observableTypes )
                 {
-                    troposphericCorrection[ std::make_pair( groundStation, source ) ][ observableType ] =
+                    auto stationSourceKey = std::make_pair( groundStation, source );
+                    auto incomingManager =
                             correctionsPerStationsAndSourcePerType.at( stationsAndSourceIt->first ).at( observableIt->first );
+
+                    if( troposphericCorrection.count( stationSourceKey ) &&
+                        troposphericCorrection.at( stationSourceKey ).count( observableType ) )
+                    {
+                        troposphericCorrection.at( stationSourceKey ).at( observableType )->mergeFrom( *incomingManager );
+                    }
+                    else
+                    {
+                        auto newManager = std::make_shared< observation_models::TabulatedMediaReferenceCorrectionManager >( );
+                        newManager->mergeFrom( *incomingManager );
+                        troposphericCorrection[ stationSourceKey ][ observableType ] = newManager;
+                    }
                 }
             }
         }
